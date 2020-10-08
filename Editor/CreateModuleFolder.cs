146cd8e2443b42136991703197cd53dd9ce8b0ea
Muhammad Salihin Bin Zaol-kefli <salsatsat@gmail.com>
2020-10-08 10:59:28 +0800

    feat: Create Module folder and Subfolders
    Create module folder with associated subfolders under Unity's Assets folder.
    Create subfolders under Unity's Assets folder.

---
+++ Editor/CreateModuleFolder.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) 2020 Muhammad Salihin Bin Zaol-kefli
+// All rights reserved.
+//
+// This software may be modified and distributed under the terms
+// of the MIT license. See the LICENSE file for details.
+//
+// Author:  Muhammad Salihin Bin Zaol-kefli  (salsatsat@gmail.com)
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateModuleFolder : ScriptableWizard
+{
+    [Header("Folder Essentials")]
+    public string moduleFolderName = "New Module Folder";
+    public List<string> subfolders = new List<string>() { "Scenes", "Scripts", "Materials", "Prefabs", "Textures" };
+
+    //
+    // Unity Methods
+    //
+
+    private void Awake()
+    {
+        // Sort list of subfolders in ascending order
+        subfolders.Sort();
+    }
+
+    //
+    // Event Methods
+    //
+
+    // Create button click
+    private void OnWizardCreate()
+    {
+        // Create primary folder
+        string guid = AssetDatabase.CreateFolder("Assets", moduleFolderName);
+        string newFolderPath = AssetDatabase.GUIDToAssetPath(guid);
+
+        // Create all the subfolders required in a module
+        foreach (string subfolder in subfolders)
+        {
+            string subGuid = AssetDatabase.CreateFolder(newFolderPath, subfolder);
+            string newSubFolderPath = AssetDatabase.GUIDToAssetPath(subGuid);
+        }
+
+        AssetDatabase.Refresh();
+    }
+
+    //
+    // Public Methods
+    //
+
+
+    //
+    // Private Methods
+    //
+
+    [MenuItem("Assets/Create/Module Folder", false, 20)]
+    private static void CreateWizard()
+    {
+        DisplayWizard("Module Folder", typeof(CreateModuleFolder), "Create");
+    }
+}
+
---
+++ Editor/CreateSubfolders.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) 2020 Muhammad Salihin Bin Zaol-kefli
+// All rights reserved.
+//
+// This software may be modified and distributed under the terms
+// of the MIT license. See the LICENSE file for details.
+//
+// Author:  Muhammad Salihin Bin Zaol-kefli  (salsatsat@gmail.com)
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateSubfolders : ScriptableWizard
+{
+    [Header("Folder Essentials")]
+    public List<string> subfolders = new List<string>() { "Scenes", "Scripts", "Materials", "Prefabs", "Textures" };
+
+    //
+    // Unity Methods
+    //
+
+    private void Awake()
+    {
+        // Sort list of subfolders in ascending order
+        subfolders.Sort();
+    }
+
+    //
+    // Event Methods
+    //
+
+    // Create button click
+    private void OnWizardCreate()
+    {
+        // Get currently selected folder
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        if (string.IsNullOrEmpty(path))
+        {
+            path = "Assets";
+        }
+        else if (!string.IsNullOrEmpty(Path.GetExtension(path)))
+        {
+            path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
+        }
+
+        // Create all the subfolders at currently selected path
+        foreach (string subfolder in subfolders)
+        {
+            string subGuid = AssetDatabase.CreateFolder(path, subfolder);
+            string newSubFolderPath = AssetDatabase.GUIDToAssetPath(subGuid);
+        }
+
+        AssetDatabase.Refresh();
+    }
+
+    //
+    // Public Methods
+    //
+
+
+    //
+    // Private Methods
+    //
+
+    [MenuItem("Assets/Create/Subfolders", false, 30)]
+    private static void CreateWizard()
+    {
+        DisplayWizard("Subfolders", typeof(CreateSubfolders), "Create");
+    }
+}
+
